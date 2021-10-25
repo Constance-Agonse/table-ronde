6c61db54c7f7d098bfd76960f5e7f25a44b029fb
Constance <constance@MacBook-Pro-de-Constance.local>
2021-10-25 17:39:07 +0200

    skill model updated

---
+++ models/Skills.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+
+
+const SkillSchema = new Schema({
+    name: {type: String, 
+        require: true},
+level: {
+            type : String ,
+            enum : ["beginner", "elementary", "intermediate", "upper intermediate", "advanced", "expert"],    
+        },
+    category: {
+        type : String ,
+        enum : ["gardening", "sports", "mechanics", "music", "cooking", "computer science", "school homework", "other"],    
+    },
+
+});
+
+
+const SkillModel = model("skills", SkillSchema);
+module.exports = SkillModel;
