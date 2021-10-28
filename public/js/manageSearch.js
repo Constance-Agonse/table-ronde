60b7f1be3022c36b1beed771b1a0f6b119f30501
mumu <lemulot78@hotmail.com>
2021-10-28 11:30:07 +0200

    axxioossssss

--- public/js/manageSearch.js
+++ public/js/manageSearch.js
@@ -1,14 +1,39 @@
-const searchBar = document.getElementById("search-skill")
+const searchBar = document.getElementById("search-skill");
+const searchLevel = document.getElementById("search-level");
+const divTest = document.getElementById("test");
+
+const searchObject = {};
 
 function searchSkill(evt) {
   axios
     .get("/home/search", {
-      params: {
-        search: evt.target.value,
-      },
+      params: searchObject,
     })
-    .then((httpResponse) => displaySkill(httpResponse.data))
+    .then((httpResponse) => {
+      console.log("Successs: ", httpResponse.data);
+      displaySkill(httpResponse.data);
+    })
     .catch((err) => console.error(err));
 }
 
-searchBar.oninput = searchSkill;
+function displaySkill(users) {
+  users.forEach((element) => {
+    divTest.innerHTML = `this is : ${element.name} ${JSON.stringify(
+      element.skills
+    )}`;
+  });
+}
+
+const handleInput = (evt) => {
+  searchObject.search = evt.target.value;
+  searchSkill();
+};
+
+const handleChange = (evt) => {
+  searchObject.level = evt.target.value;
+  searchSkill();
+};
+
+
+searchBar.oninput = handleInput;
+searchLevel.onchange = handleChange;
