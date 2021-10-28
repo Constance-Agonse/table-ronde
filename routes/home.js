e3ed3bdec1fff59e78508e48724650938fb706b6
Hugo Lansade <lhugo@lansade.com>
2021-10-28 12:12:06 +0200

    take constance work

--- routes/home.js
+++ routes/home.js
@@ -16,7 +16,7 @@ const skillsConvert = (currentValue) => {
   } else if (index < 2) {
     return ["starter", "elementary", "intermediate"]
   } else if (index < 1) {
-    return ["starter", "elementary", "intermediate"]
+    return ["starter", "elementary"]
   }
 };
 
@@ -25,36 +25,6 @@ router.get("/", (req, res, next) => {
 });
 
 router.get("/search", async function (req, res, next) {
-<<<<<<< HEAD
-    // try {
-    //     const test = await UserModel.findOne().populate("skills")
-    //     console.log("===========");
-    //     console.log(test.skills);
-       
-    // } catch (error) {
-        
-    // }
-    console.log("=====");
-    console.log(req.query.search)
-    const regexp = new RegExp(req.query.search, "ig");
-    console.log(regexp);
-    {skills: `"${req.query.search}"` 
-          }
-
-    // const test = UserModel
-    // .find({skills: { $regex: regexp } 
-    //   })
-    //   .then((dbRes) => {
-    //     console.log("=====");
-    //     console.log(test);
-    //     res.json(dbRes)} )
-    //   .catch(next);
-  });
-
-
-
-module.exports = router;
-=======
   //   const regexp = new RegExp(req.query.search, "ig");
 
   const regexp = new RegExp(req.query.search, "ig");
@@ -69,9 +39,22 @@ module.exports = router;
     // console.log("========");
     // console.log(test);
     const test3 = [];
-    test2 = test.forEach(function (result) {
+    test.forEach(function (result) {
       if (result.skills.length) {
-        test3.push(result);
+        console.log("******1*****")        
+        console.log(result.skills)
+        console.log("*****2******")
+        result.skills.foreach((skill) => {
+          console.log("///////////")
+          console.log(skill.level)
+        })
+        // if(result.skills[0].level === "expert"){
+        //   console.log("*****3******")
+        // console.log(result)          
+          test3.push(result);
+        // }
+        
+        
       }
     });
 
@@ -89,4 +72,3 @@ module.exports = router;
 });
 
 module.exports = router;
->>>>>>> 60b7f1be3022c36b1beed771b1a0f6b119f30501
